6eb2482a323e56de18c8d762818b24e7ed65184c
Arthur-Candeia <arthurheher@hotmail.com>
2023-08-12 12:24:53 -0300

    correcao na funcao de deletar itens

--- src/components/Item/deleteItem.ts
+++ src/components/Item/deleteItem.ts
@@ -2,7 +2,7 @@ import item from '../../interfaces/itemInterface'
 
 export default function deleteItem(allItems: item[], index: string | undefined) {
   if (confirm(`Deseja realmente excluir o item ${allItems[Number(index)].name}?`)) {
-    allItems.splice(Number(index));
+    allItems.splice(1, Number(index));
     localStorage.allItems = JSON.stringify(allItems)
   }
 }
