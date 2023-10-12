b5f51e4fb99436a7cf00fe4f62bc7a81e052db70
Arthur-Candeia <arthurheher@hotmail.com>
2023-10-12 15:17:57 -0300

    FIX: Correção de erro na exclusão de itens do stock

--- src/components/Item/deleteItem.ts
+++ src/components/Item/deleteItem.ts
@@ -2,7 +2,7 @@ import item from '../../interfaces/itemInterface'
 
 export default function deleteItem(allItems: item[], index: string | undefined) {
   if (confirm(`Deseja realmente excluir o item ${allItems[Number(index)].name}?`)) {
-    allItems.splice(1, Number(index));
+    allItems.splice(Number(index), 1);
     localStorage.allItems = JSON.stringify(allItems)
   }
 }
